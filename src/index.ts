60d3e89c2c849d6a8c647ea292d30b3ad4cc0dae
Tobi <=>
2021-02-28 22:45:18 -0600

    Some default stuff added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
-import { app } from 'electron'
+import { 
+    app, 
+    BrowserWindow
+} from 'electron'
+
+
+var MainWindow: BrowserWindow;
+
+const screen: Function = function (name: string): {index: string} {
+    return {
+        index: `./src/screens/${name}/index.html`
+    }
+}
+
+const CreateWindow: Function = async function (path?: string): Promise<void> {
+    if (!MainWindow) {
+        MainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            resizable: false,
+            webPreferences: {
+                devTools: true,
+                nodeIntegration: true,
+                enableRemoteModule: true,
+                nativeWindowOpen: true
+            }
+        })
+    }
+    MainWindow.loadFile(path || screen('main').index)
+}
 
 app.on('ready', () => {
-    console.log("ready")
+    CreateWindow()
 })
+
